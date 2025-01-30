8b5b405b4998e5400af5dff2aa6daff77d877e0e
vjcitn <stvjc@channing.harvard.edu>
2025-01-30 06:47:45 -0500

    adds flexibility to spatial neighbor graph calculation

--- R/process_mcmicro.R
+++ R/process_mcmicro.R
@@ -6,8 +6,12 @@
 #' @import Voyager
 #' @param h5ad character(1) path to an MCMICRO output in h5ad format
 #' @param coordnames character() names in colData of transformed h5ad used to specify X, Y
+#' @param spneigh_parms list() of arguments (other than x) to Voyager::findSpatialNeighbors, defaults to list()
 #' @param assay4quants character(1) name in assayNames of transformed h5ad used as protein quantification
 #' @param verbose logical(1) will message as stages complete if TRUE
+#' @note Voyager::findSpatialNeighbors uses methods in the spdep package to define neighbors.  In this
+#' function, we compute a `colGraph` component, which assumes MARGIN value is 2.  An example
+#' spneigh_parms setting is `list(method='knearneigh', k=4)`.
 #' @examples
 #' pa = get_mcm_path("m53.1")
 #' mcm53.1 = process_mcmicro(pa)
@@ -15,9 +19,14 @@
 #' Voyager::plotLocalResult(mcm53.1, "localG", features = features_use,
 #'                 colGeometryName = "centroids", divergent = TRUE,
 #'                 diverge_center = 0)
+#' mcm53.1b = process_mcmicro(pa, spneigh_parms=list(method="knearneigh", k=3))
+#' Voyager:::plotColGraph(mcm53.1b)
+#' Voyager::plotLocalResult(mcm53.1b, "localG", features = features_use,
+#'                 colGeometryName = "centroids", divergent = TRUE,
+#'                 diverge_center = 0)
 #' @export
 process_mcmicro = function(h5ad,
-     coordnames = c("X_centroid", "Y_centroid"), assay4quants="X", verbose=TRUE,
+     coordnames = c("X_centroid", "Y_centroid"), spneigh_parms=list(), assay4quants="X", verbose=TRUE,
      ...) {
   sce = zellkonverter::readH5AD(h5ad)
   spe = as(sce, "SpatialExperiment")
@@ -27,7 +36,8 @@ process_mcmicro = function(h5ad,
   stopifnot(assay4quants %in% assayNames(sfe))
   allfeat = rownames(sfe)
   if (verbose) message("findSpatialNeighbors")
-  g1 = findSpatialNeighbors(sfe) 
+  fsnargs = list(x=sfe)
+  g1 = do.call(findSpatialNeighbors, c(fsnargs, spneigh_parms))
   colGraph(sfe, "spatNeigh") = g1 
   if (verbose) message("runUnivariate moran.plot")
   sfe = runUnivariate(sfe, type="moran.plot", features=allfeat, 
