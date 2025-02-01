e274806db582ed53d675fa01dd2046932a23b3f3
vjcitn <stvjc@channing.harvard.edu>
2025-02-01 10:33:10 -0500

    added more functionality to app2

--- inst/appdev/app2.R
+++ inst/appdev/app2.R
@@ -3,6 +3,12 @@ aa = c("m53.1.sfe", "m53.2.sfe", "m53.3.sfe", "m53.4.sfe", "m62.1.sfe")
 fis = paste(fol, aa, sep="/")
 names(fis) = c("m53.1", "m53.2", "m53.3", "m53.4", "m62.1")
 
+targets = c("AF488", "AF555", "AF647", "A488_background", "A555_background", 
+"A647_background", "FDX1", "CD357", "CD1D", "CD163", "CD3D", 
+"CD31", "LDH", "CD66B", "VDAC1", "ELANE", "CD57", "CD45", "CD11B", 
+"SMA", "CD16", "ECAD", "FOXP3", "NCAM", "CD4", "KERATIN", "CD14", 
+"IBA1", "CD1B", "CD8A")
+
 library(GBCC.mcm)
 library(alabaster.sfe)
 #fis = GBCC.mcm:::h5ads
@@ -13,31 +19,40 @@ ui = fluidPage(
  sidebarPanel(
   helpText("MCMICRO samples"),
   radioButtons("samples", "samples",
-    fis), width=2
+    fis), 
+  width=2
   ),
  mainPanel(
   tabsetPanel(
    tabPanel("basic",
-    plotOutput("simple")
+    plotOutput("simple"),
+    uiOutput("checkboxes")
    ),
-   tabPanel("basic",
-    plotOutput("graph")
+   tabPanel("neighbors",
+    plotOutput("graph"),
    ),
    tabPanel("about",
-    verbatimTextOutput("iniabout")
+    textOutput("iniabout"),
+    helpText("sources at github.com/vjcitn/GBCC.mcm"),
+    verbatimTextOutput("cursfe")
    )
   )
  )
 )
 
 server = function(input, output) {
- output$iniabout = renderPrint(
-  sprintf("GBCC.mcm ver. %s", packageVersion("GBCC.mcm"))
+ output$cursfe = renderPrint({
+  cur = readObject(input$samples)
+  cur
+ })
+ output$iniabout = renderText(
+  sprintf("GBCC.mcm version. %s", packageVersion("GBCC.mcm"))
  )
 
  output$simple = renderPlot({
    cur = readObject(input$samples)
-   features_use = c("NCAM", "FOXP3", "CD8A", "LDH")
+   validate(need(isTRUE(nchar(input$genes2[1])>0), "awaiting gene list"))
+   features_use = input$genes2
    Voyager::plotLocalResult(cur, "localG", features = features_use,
                    colGeometryName = "centroids", divergent = TRUE,
                    diverge_center = 0)
@@ -45,6 +60,10 @@ server = function(input, output) {
  output$graph = renderPlot({
    cur = readObject(input$samples)
    Voyager::plotColGraph(cur)
+   })
+ output$checkboxes = renderUI({
+   checkboxGroupInput("genes2", "targ2", choices=targets,
+       selected=c("NCAM", "FOXP3", "CD8A", "LDH"), inline=TRUE)
    })
 }
 
